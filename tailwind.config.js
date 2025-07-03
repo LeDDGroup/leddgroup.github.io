e454e920d9fe5b9b3dacc6f66e31abf19b931129
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-07-03 13:46:12 -0700

    Add unocss

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}", "./index.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
